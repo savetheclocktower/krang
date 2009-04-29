16aa6e9a2f0135aa4a5f2399dcaa2c1979cab9c1
Andrew Dupont <prototype@andrewdupont.net>
2009-04-28 19:43:34 -0500

    Added Dataset.Table for pulling data from an HTML table on the page.

---
+++ src/dataset/table.js
@@ -0,0 +1,33 @@
+
+
+Dataset.Table = Class.create(Dataset.Base, Krang.Mixin.Configurable, {
+  initialize: function(element, options) {
+    this.element = $(element);
+    this.setOptions(options);
+    var opt = this.options;
+    
+    if (opt.hideTable) this.element.hide();
+    
+    this.labels = this.element.select(opt.labels).map( function(node) {
+      return opt.labelFilter(node.innerHTML);
+    });
+    this.values = this.element.select(opt.values).map( function(node) {
+      return opt.valueFilter(node.innerHTML);
+    });
+  }
+});
+
+
+Object.extend(Dataset.Table, {
+  DEFAULT_OPTIONS: {
+    hideTable: true,
+    
+    labels: 'tbody > tr > td:first-child',
+    values: 'tbody > tr > td:last-child',
+    
+    labelFilter: Prototype.K,
+    valueFilter: function(text) {
+      return Number(text);
+    }
+  }
+});
