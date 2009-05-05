285515921a6804ef8767deb5494fdfef75a4b9e9
Andrew Dupont <prototype@andrewdupont.net>
2009-05-05 10:52:09 -0500

    Make Krang.deepExtend more readable.

--- src/mixins.js
+++ src/mixins.js
@@ -1,13 +1,18 @@
 Krang.deepExtend = function(destination, source) {
   for (var property in source) {
     var type = typeof source[property], deep = true;
-        
-    if (type !== 'object' || Object.isElement(source[property]) ||
-     (source[property] && source[property].constructor &&
-      source[property].constructor !== Object)) {
-       deep = false;
-    }
     
+    if (source[property] === null || type !== 'object')
+      deep = false;
+      
+    if (Object.isElement(source[propety]))
+      deep = false;
+      
+    if (source[property] && source[property].constructor &&
+     source[property].constructor !== Object) {
+      deep = false;
+    } 
+        
     if (deep) {
       destination[property] = destination[property] || {};
       arguments.callee(destination[property], source[property]);
