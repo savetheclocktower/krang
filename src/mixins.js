f7d4754a71a937de7dc2dca8a941f581a5dbcb72
Andrew Dupont <prototype@andrewdupont.net>
2009-08-11 23:46:18 -0500

    Update `Krang.Mixin#setOptions` so that it can be called more than once without overriding existing options.

--- src/mixins.js
+++ src/mixins.js
@@ -5,7 +5,6 @@
  *  
  *  Used within Krang to blend user-set options with the defaults.
 **/
-
 Krang.deepExtend = function(destination, source) {
   for (var property in source) {
     var type = typeof source[property], deep = true;
@@ -31,6 +30,9 @@ Krang.deepExtend = function(destination, source) {
   return destination;
 };
 
+/**
+ *  Krang.Mixin
+**/
 Krang.Mixin = {};
 
 /**
@@ -51,21 +53,22 @@ Krang.Mixin.Configurable = {
    *  Sets options on the class.
   **/
   setOptions: function(options) {
-    this.options = {};
-    var constructor = this.constructor;
-    
-    if (constructor.superclass) {
-      // Build the inheritance chain.
-      var chain = [], klass = constructor;
-      while (klass = klass.superclass)
-        chain.push(klass);
-      chain = chain.reverse();
-      
-      for (var i = 0, l = chain.length; i < l; i++)
-        Krang.deepExtend(this.options, chain[i].DEFAULT_OPTIONS || {});
+    if (!this.options) {
+      this.options = {};
+      var constructor = this.constructor;    
+      if (constructor.superclass) {
+        // Build the inheritance chain.
+        var chain = [], klass = constructor;
+        while (klass = klass.superclass)
+          chain.push(klass);
+        chain = chain.reverse();
+
+        for (var i = 0, l = chain.length; i < l; i++)
+          Krang.deepExtend(this.options, chain[i].DEFAULT_OPTIONS || {});
+      }
+
+      Krang.deepExtend(this.options, constructor.DEFAULT_OPTIONS || {});
     }
-    
-    Krang.deepExtend(this.options, constructor.DEFAULT_OPTIONS || {});
     return Krang.deepExtend(this.options, options || {});
   }
 };
